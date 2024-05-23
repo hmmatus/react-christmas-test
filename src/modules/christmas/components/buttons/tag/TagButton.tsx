2ad71627cc83d1fc8f3573a2d296c74b26e61de4
Hector Matus <{ID}+{username}@users.noreply.github.com>
2024-05-23 14:41:54 -0600

    feature add list of products when press see products

--- src/modules/christmas/components/buttons/tag/TagButton.tsx
+++ src/modules/christmas/components/buttons/tag/TagButton.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import { CatalogTagI } from "../../../models/catalog.model";
 import "./Tag.scss";
+import TagIcon from "./TagIcon";
 type TagButtonProps = {
   tag: CatalogTagI;
+  onClick: () => void;
 };
-const TagButton = ({ tag }: TagButtonProps) => {
-  const onRedirect = () => {
-    window.open(tag.url, "_blank", "noopener,noreferrer");
+const TagButton = ({ tag, onClick }: TagButtonProps) => {
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseOver = () => {
+    setIsHovering(true);
   };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
     <button
-      onClick={onRedirect}
+      onMouseEnter={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      onClick={onClick}
       className="tag-button"
       style={{
         position: "absolute",
@@ -19,7 +30,11 @@ const TagButton = ({ tag }: TagButtonProps) => {
         bottom: tag.position.bottom,
       }}
     >
-      Tag
+      <TagIcon
+        className="tag-icon"
+        fillColor={isHovering ? "#EF3742" : "#fff"}
+        fillCartColor={isHovering ? "#fff" : "#EF3742"}
+      />
     </button>
   );
 };
--- src/modules/christmas/components/catalog/catalogList/CatalogListComponent.tsx
+++ src/modules/christmas/components/catalog/catalogList/CatalogListComponent.tsx
@@ -1,7 +1,11 @@
+import { useQuery } from "react-query";
 import { CatalogItemI } from "../../../models/catalog.model";
 import TagButton from "../../buttons/tag/TagButton";
 import CatalogItem from "../catalogItem/CatalogItem";
 import "./CatalogList.scss";
+import { useEffect, useState } from "react";
+import CatalogCarrousel from "../catalogCarrousel/CatalogCarrousel";
+import ProductCard from "../../../../../components/elements/cards/productCard/ProductCard";
 
 type CatalogListComponentProps = {
   catalog: CatalogItemI;
@@ -13,11 +17,32 @@ const CatalogListComponent = ({
   index,
   className,
 }: CatalogListComponentProps) => {
+  const [isCatalogOpen, setIsCatalogOpen] = useState(false);
+  const { isLoading, isError, data, refetch } = useQuery(
+    "catalogCategories",
+    () => {
+      return fetch("https://fakestoreapi.com/products").then((res) =>
+        res.json()
+      );
+    }
+  );
   const onRedirect = (url: string) => {
     window.location.replace(url);
   };
+  const handleSeeProducts = () => {
+    setIsCatalogOpen(true);
+    refetch();
+  };
+
+  const onCloseProductsView = () => {
+    setIsCatalogOpen(false);
+  };
+
+  useEffect(() => {
+    console.log("isCatalogOpen", isCatalogOpen);
+  }, [isCatalogOpen]);
   return (
-    <div className={`catalog-list-container ${className}`}>
+    <div className={`catalog-list-container ${className ?? ""}`}>
       <div className="banner">
         <img src={catalog.bannerImg} alt={`banner ${index}`} />
       </div>
@@ -25,12 +50,24 @@ const CatalogListComponent = ({
         className="main"
         style={{ backgroundImage: `url(${catalog.main.background})` }}
       >
-        <div className="tags">
-          {catalog.main.tags.map((tag, index) => (
-            <TagButton key={index} tag={tag} />
-          ))}
+        <div className="main-content">
+          {isCatalogOpen ? (
+            <CatalogCarrousel
+              data={data}
+              loading={isLoading}
+              onClose={onCloseProductsView}
+            />
+          ) : (
+            <>
+              {catalog.main.tags.map((tag, index) => (
+                <TagButton key={index} tag={tag} onClick={handleSeeProducts} />
+              ))}
+              <button className="products-button" onClick={handleSeeProducts}>
+                Ver productos
+              </button>
+            </>
+          )}
         </div>
-        <button className="products-button">Ver productos</button>
       </div>
       <div className="popular-categories">
         <div className="grid-rows-2">
