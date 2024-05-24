4ed5aad740fdc561e686708c1c0df1c79dd9ea42
Hector Matus <{ID}+{username}@users.noreply.github.com>
2024-05-23 21:31:36 -0600

    feature add footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import NavBar from "./components/modules/navbar/Navbar";
 import ChristmasHeader from "./modules/christmas/components/header/Header";
 import ChristMasCatalogDisplay from "./modules/christmas/components/catalog/Catalog";
 import NewRelease from "./modules/christmas/components/newRelease/NewReleaseSection";
+import Footer from "./components/modules/footer/Footer";
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
         <ChristMasCatalogDisplay />
         <NewRelease />
       </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/modules/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Footer.scss";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-information">
+        <p>Almacenes Siman S.A. de C.V. - NIT: 0614–170266–001-3</p>
+        <p>
+          Almacenes venta de diversos artículos - Paseo General Escalón, Centro
+          Comercial Galerías, San Salvador.
+        </p>
+        <p>Llámanos 2298-3777 - contacto@siman.com</p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
